40ef98b9af28cb79cf3e05797ff0c0e25f3c5e00
audh25-lab <audh25@gmail.com>
2025-12-21 22:16:32 +0500

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+ for (let x = zone.x; x < zone.x + zone.w && x < map.width; x++) {
 // ============================================
 // RAAJJE THEFT AUTO 1: THE BAOKALO CHRONICLES
 // Complete Game Engine - Act 1: Maafannu Awakening
